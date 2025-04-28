a72c07ef213d0e24751efd5d2e7e2f7ac7ab3691
Leonardo Benedetti Antunes <leonardobenedettiantunes@gmail.com>
2025-04-27 22:36:43 -0300

    feat: adiciona o dto

--- src/main/ecommerce/core/domain/entity/Carrinho.java
+++ src/main/ecommerce/core/domain/entity/Carrinho.java
@@ -5,4 +5,33 @@ import java.util.List;
 public class Carrinho {
     List<Produto> produtos;
     float valorTotal;
+
+    public Carrinho(List<Produto> produtos, float valorTotal) {
+        this.produtos = produtos;
+        this.valorTotal = valorTotal;
+    }
+
+    public List<Produto> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<Produto> produtos) {
+        this.produtos = produtos;
+    }
+
+    public float getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(float valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    @Override
+    public String toString() {
+        return "Carrinho{" +
+                "produtos=" + produtos +
+                ", valorTotal=" + valorTotal +
+                '}';
+    }
 }
